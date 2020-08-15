693965a56ca9722f8acad9be4789145ace9b5a4a
Rock de Vocht <peter@peter.co.nz>
2020-08-15 11:14:32 +0100

    fixed deletion and added more comments

--- README.md
+++ README.md
@@ -3,12 +3,45 @@ Written in Python 3, based on https://github.com/dhamya/docker-registry-cli
 
 usage:
 ```
-./docker-private.py -s 129.157.181.199:5000 -u admin -p p@ssw0rd
+./docker-private.py -s https://129.157.181.199:5000 -u admin -p p@ssw0rd
 
 # or
 
-./docker-private.py -s 129.157.181.199:5000
+./docker-private.py -s https://129.157.181.199:5000
 ```
 
 ## Delete function does not garbage collect
 see https://docs.docker.com/registry/garbage-collection/
+
+## Running a private repo
+To run a private repo with certs locally on port 8443
+```
+docker run -d \
+  --restart=always \
+  --name registry \
+  -v /docker-registry:/var/lib/registry \
+  -v "$(pwd)"/certs:/certs \
+  -e REGISTRY_HTTP_ADDR=0.0.0.0:443 \
+  -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/cert-bundle.txt \
+  -e REGISTRY_HTTP_TLS_KEY=/certs/key.txt \
+  -e REGISTRY_STORAGE_DELETE_ENABLED="true" \
+  -p 8443:443 \
+  registry:2
+```
+To run a private repo without certs locally on port 8080
+```
+docker run -d \
+  --restart=always \
+  --name registry \
+  -v /docker-registry:/var/lib/registry \
+  -e REGISTRY_HTTP_ADDR=0.0.0.0:8000 \
+  -e REGISTRY_STORAGE_DELETE_ENABLED="true" \
+  -p 8080:8000 \
+  registry:2
+```
+
+## adding files to your private repo
+```
+docker tag local-item:1.0.0 localhost:8443/local-item:1.0.0
+docker push localhost:8443/local-item:1.0.0
+```
--- docker-private.py
+++ docker-private.py
@@ -19,12 +19,12 @@ exclude_list = list()
 def usage():
     print('')
     print('Usage: ' + sys.argv[0] + ' [-s server] [-u username] [-p password] [-v verbose] [-d delete] [-e EXCLUDE]')
-    print('  -s   --server            Docker registry URL')
+    print('  -s   --server            Docker registry URL, prefixed with http:// or https://')
     print('  -u   --username          docker registry user-name')
     print('  -p   --password          docker registry user\'s password')
     print('  -d   --delete            a specific image name:tag format')
     print('')
-    print('example: ' + sys.argv[0] + ' -s 129.157.181.199:5000 -u admin -p p@ssword -d name:1.0.0')
+    print('example: ' + sys.argv[0] + ' -s http://129.157.181.199:5000 -u admin -p p@ssword -d name:1.0.0')
 
 
 def make_request(url, method):
@@ -32,18 +32,33 @@ def make_request(url, method):
 
     try:
         if method == "DELETE":
+            # ignore 404s as they mean it is already gone
             if len(_AUTH_TOKEN) > 0:
-                return requests.delete(url, headers={"Authorization": "Basic %s" % _AUTH_TOKEN})
+                r = requests.delete(url, headers={"Authorization": "Basic %s" % _AUTH_TOKEN,
+                                                  "Accept": "application/vnd.docker.distribution.manifest.v2+json"})
+                if r.status_code not in range(200, 299) and r.status_code != 404:
+                    raise ValueError("\"%s\":DELETE returned status code %s" % (url, r.status_code))
+                return r
             else:
-                return requests.delete(url)
+                r = requests.delete(url, headers={"Accept": "application/vnd.docker.distribution.manifest.v2+json"})
+                if r.status_code not in range(200, 299) and r.status_code != 404:
+                    raise ValueError("\"%s\":DELETE returned status code %s" % (url, r.status_code))
+                return r
         else:
             if len(_AUTH_TOKEN) > 0:
-                return requests.get(url, headers={"Authorization": "Basic %s" % _AUTH_TOKEN})
+                r = requests.get(url, headers={"Authorization": "Basic %s" % _AUTH_TOKEN,
+                                               "Accept": "application/vnd.docker.distribution.manifest.v2+json"})
+                if r.status_code not in range(200, 299):
+                    raise ValueError("\"%s\":GET returned status code %s" % (url, r.status_code))
+                return r
             else:
-                return requests.get(url)
+                r = requests.get(url, headers={"Accept": "application/vnd.docker.distribution.manifest.v2+json"})
+                if r.status_code not in range(200, 299):
+                    raise ValueError("\"%s\":GET returned status code %s" % (url, r.status_code))
+                return r
 
-    except ValueError:
-        print("Unknown error occurred during execution")
+    except ValueError as ex:
+        print("Unknown error occurred during execution: %s" % ex)
 
 
 # append spaces to a string to make it even length
@@ -58,6 +73,71 @@ def postfix(string, length):
 def pretty_print(repo, tag, sha1):
     string_list = [postfix(repo, 30), postfix(tag, 20), sha1]
     print(''.join(string_list))
+
+
+# get details on a repo item
+def get_repo_item_details(repo):
+    item_detail_list = []
+    global _SERVER, _TAGS
+    _url = _SERVER + repo + _TAGS
+    result = make_request(_url, "GET")
+    tags = result.json()
+    try:
+        for tag in tags["tags"]:
+            _url = _SERVER + repo + _MANIFEST + tag
+            result = make_request(_url, "GET")
+            sha1 = result.headers[_DOCKER_CONT_HDR]
+            item_detail_list.append((repo, tag, sha1, result.json()))
+    except TypeError:
+        pass
+    return item_detail_list
+
+
+# list all items in a repo
+def list_repo():
+    global _SERVER, _CATALOG
+
+    _url = _SERVER + _CATALOG
+    result = make_request(_url, "GET")
+    repos = result.json()
+    num_repos = 0
+    for repo in repos["repositories"]:
+        item_list = get_repo_item_details(repo)
+        for _, tag, sha1, _ in item_list:
+            pretty_print(repo, tag, sha1)
+            num_repos += 1
+    if num_repos == 0:
+        print("empty repository")
+
+
+# delete a specific name:version item
+def delete_repo_item(repo, tag):
+    # get all the versions for this item
+    item_list = get_repo_item_details(repo)
+    found = False
+    delete_set = set()
+    for _, item_tag, sha1, fs_layers in item_list:
+        # can we find the one that matches?
+        if tag == item_tag:
+            found = True
+            if sha1 not in delete_set:
+                delete_set.add(sha1)
+                _url = _SERVER + repo + _MANIFEST + sha1
+                print(sha1)
+                make_request(_url, "DELETE")
+                if _FS_LAYERS in fs_layers:
+                    for fs_layer in fs_layers[_FS_LAYERS]:
+                        sha1_layer = fs_layer[_BLOB_SUM]
+                        if sha1_layer not in delete_set:
+                            delete_set.add(sha1_layer)
+                            _url = _SERVER + repo + _MANIFEST + sha1_layer
+                            print(sha1_layer)
+                            make_request(_url, "DELETE")
+    if not found:
+        print("could not find %s:%s" % (repo, tag))
+    else:
+        print("make sure you run:")
+        print("  docker exec registry bin/registry garbage-collect /etc/docker/registry/config.yml")
 
 
 def main():
@@ -86,7 +166,10 @@ def main():
             sys.exit(2)
         elif opt in ('-s', '--server'):
             global _SERVER
-            _SERVER = "https://" + arg + "/v2/"
+            if not arg.startswith("http://") and not arg.startswith("https://"):
+                print("server url must start with http:// or https://")
+                sys.exit(2)
+            _SERVER = arg + "/v2/"
         elif opt in ('-u', '--username'):
             username = arg
         elif opt in ('-p', '--password'):
@@ -94,6 +177,9 @@ def main():
         elif opt in ('-d', '--delete'):
             delete = True
             image_to_delete = arg
+            if len(image_to_delete.strip()) < 3 or ":" not in image_to_delete:
+                print("delete error: requires name:tag format")
+                sys.exit(2)
         else:
             usage()
             sys.exit(2)
@@ -108,26 +194,12 @@ def main():
     result = make_request(_url, "GET")
     repos = result.json()
 
-    for repo in repos["repositories"]:
-        _url = _SERVER + repo + _TAGS
-        result = make_request(_url, "GET")
-        tags = result.json()
-        try:
-            for tag in tags["tags"]:
-                _url = _SERVER + repo + _MANIFEST + tag
-                result = make_request(_url, "GET")
-                # fs_layers = result.json()
-                sha1 = result.headers[_DOCKER_CONT_HDR]
-                _lookupStr = repo + ":" + tag
-                if delete and _lookupStr == image_to_delete:
-                    print("removing " + repo + ":" + tag)
-                    _url = _SERVER + repo + _MANIFEST + sha1
-                    make_request(_url, "DELETE")
-                else:
-                    pretty_print(repo, tag, sha1)
-
-        except TypeError:
-            print("**** No tag found for this image/repo")
+    if delete:
+        repo = image_to_delete.split(':')[0]
+        tag = image_to_delete.split(':')[1]
+        delete_repo_item(repo.strip(), tag.strip())
+    else:
+        list_repo()
 
 
 if __name__ == "__main__":
